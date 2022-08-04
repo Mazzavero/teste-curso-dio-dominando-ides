765b91b10ac430bc9e4cc7e9eeed70c30efb4f6f
veronica <veronica.bmazza@gmail.com>
2022-08-03 21:14:42 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        //Criando a classe
+        Gato gato = new Gato();
+
+    }
+}
