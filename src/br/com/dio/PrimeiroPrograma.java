5be91a39d95f5e37cc843bc2ffe7986390e78c8b
veronica <veronica.bmazza@gmail.com>
2022-08-03 21:41:04 -0300

    changes livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,5 +8,42 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro = new Livro("O problema dos sete corpos", 300);
+        System.out.println(livro);
+
+    }
+
+    static class Livro {
+        private String nome;
+        private Integer numpag;
+
+        public Livro(String nome, Integer numpag) {
+            this.nome = nome;
+            this.numpag = numpag;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumpag() {
+            return numpag;
+        }
+
+        public void setNumpag(Integer numpag) {
+            this.numpag = numpag;
+        }
+
+        @Override
+        public String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numpag=" + numpag +
+                    '}';
+        }
     }
 }
